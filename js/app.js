a150c778806b64ec9affa24e2badc9b99d4e8282
Joshua Frazer <joshua.frazer.school@gmail.com>
2022-06-08 15:26:01 -0700

    refactored code & added two minigame questions

--- js/app.js
+++ js/app.js
@@ -1,82 +1,77 @@
 'use strict';
 
 let userName = prompt('What is your name?');
-//console.log('Hello, ' + userName + '.');
 alert('Hello, ' + userName + '.');
 
-let answer = prompt('Is Josh\'s last name spelled "Frazier"? \n Enter "Y" or "y" for Yes, "N" or "n" for No');
-answer = answer.toLowerCase();
-if (answer === 'y') {
-  //console.log('Incorrect! Please stop doing that.');
-  alert('Incorrect! Please stop doing that.');
-}
-else {
-  //console.log('Correct. It\'s "Frazer".');
-  alert('Correct. It\'s "Frazer".');
-}
+let userScore = 0;
 
-answer = prompt('Has Josh ever lived in India? \n Enter "Y" or "y" for Yes, "N" or "n" for No');
-answer = answer.toLowerCase();
-if (answer === 'y') {
-  //console.log('Incorrect, not yet anyway.');
-  alert('Incorrect, not yet anyway.');
-}
-else {
-  //console.log('Correct, but he\'d like to visit.');
-  alert('Correct, but he\'d like to visit.');
-}
+let questionsAndAnswers = [['Is Josh\'s last name spelled "Frazier"?', 'n'],
+  ['Has Josh ever lived in India?', 'n'],
+  ['Has Josh ever lived in Korea?', 'y'],
+  ['Does Josh like football?', 'n'], 
+  ['Has Josh ever eaten balut?', 'y']];
 
-answer = prompt('Has Josh ever lived in Korea? \n Enter "Y" or "y" for Yes, "N" or "n" for No');
-answer = answer.toLowerCase();
-if (answer === 'y') {
-  //console.log('Correct. He lived there for two years.');
-  alert('Correct. He lived there for two years.');
-}
-else {
-  //console.log('Incorrect, he did live there.');
-  alert('Incorrect, he did live there.');
-}
+let correctAndIncorrectResponses = [['Correct. It\'s "Frazer".', 'Incorrect! Please stop doing that.'],
+  ['Correct, but he\'d like to visit.', 'Incorrect, not yet anyway.'],
+  ['Correct. He lived there for two years.', 'Incorrect, he did live there.'],
+  ['Correct. Josh is not interested in football.', 'Incorrect. Josh doesn\'t watch football.'],
+  ['Correct. It tastes a bit like duck soup.', 'Incorrect. Josh enjoyed balut.']];
 
-answer = prompt('Does Josh like football? \n Enter "Y" or "y" for Yes, "N" or "n" for No');
-answer = answer.toLowerCase();
-if (answer === 'y') {
-  //console.log('Incorrect. Josh doesnt watch football.');
-  alert('Incorrect. Josh doesnt watch football.');
+for (let questionNumber = 0; questionNumber < questionsAndAnswers.length && questionNumber < correctAndIncorrectResponses.length; questionNumber++) {
+  let question = questionsAndAnswers[questionNumber][0];
+  let correctAnswer = questionsAndAnswers[questionNumber][1];
+  let correctResponse = correctAndIncorrectResponses[questionNumber][0];
+  let incorrectResponse = correctAndIncorrectResponses[questionNumber][1];
+  let userAnswer = prompt(`${question}\n Enter "Y" or "y" for Yes, "N" or "n" for No`).toLowerCase();
+  if (userAnswer === correctAnswer) {
+    alert(correctResponse);
+    userScore += 1;
+  }
+  else {
+    alert(incorrectResponse);
+  }
 }
-else {
-  //console.log('Correct. Josh is not interested in football.');
-  alert('Correct. Josh is not interested in football.');
-}
-
-answer = prompt('Has Josh ever eaten balut? \n Enter "Y" or "y" for Yes, "N" or "n" for No');
-answer = answer.toLowerCase();
-if (answer === 'y') {
-  //console.log('Correct. It tastes a bit like duck soup.');
-  alert('Correct. It tastes a bit like duck soup.');
-}
-else {
-  //console.log('Incorrect. Josh enjoyed balut.');
-  alert('Incorrect. Josh enjoyed balut.');
-}
-
-alert('Thanks for playing, ' + userName + '.'); 
 
 let numberAnswer = Math.floor(Math.random() * 10) + 1;
-for(let attempts = 0; attempts < 4; attempts++) {
+for (let attempts = 4; attempts > 0; attempts--) {
   let userAnswer = parseInt(prompt('Guess a number between 1 and 10'));
   if (userAnswer === numberAnswer) {
     alert('Correct, the number was ' + numberAnswer);
+    userScore += 1;
     break;
   }
   else {
-    if (userAnswer > numberAnswer) {
-      alert('You guessed too high');
+    if (attempts === 1) {
+      alert(`Sorry, that was your last try. The number was ${numberAnswer}`);
+      break;
     }
-    if (userAnswer < numberAnswer) {
-      alert('You guessed too low');
+    if (userAnswer > numberAnswer) {
+      alert(`You guessed too high. You have ${attempts - 1} tries remaining.`);
+    }
+    else {
+      alert(`You guessed too low. You have ${attempts - 1} tries remaining.`);
     }
   }
 }
 
-alert(`The number was ${numberAnswer}`);
+let possibleAnswers = ['Japan', 'Korea', 'South Korea', 'Qatar', 'United Arab Emirates', 'UAE', 'Philippines', 'The Philippines', 'Taiwan', 'Germany', 'Canada', 'Greece', 'UK', 'United Kingdom', 'The UK', 'The United Kingdom', 'Britain', 'England'];
+let userCorrect = false;
+for (let attempt = 6; attempt > 0; attempt--){
+  let userAnswer = prompt('Try naming a country (besides the US )that Josh has been to: ').toLowerCase();
+  for (let index = 0; index < possibleAnswers.length; index++) {
+    if (userAnswer === possibleAnswers[index].toLowerCase()) {
+      alert(`Correct, Josh has been to ${possibleAnswers[index]}`);
+      userCorrect = true;
+    }
+  }
+  if (userCorrect) {
+    userScore += 1;
+    break;
+  }
+  else {
+    alert('Sorry, that is not in the list of correct answers. You have ' + (attempt - 1) + ' tries remaining. Please try again.');
+  }
+}
+alert('The possible answers were: ' + possibleAnswers);
 
+alert(`${userName}, your score was ${userScore}. Thanks for playing.`);
