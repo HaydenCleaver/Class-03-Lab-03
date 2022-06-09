9c1117768b29810e42e6cde5e8db6a6a527f4d66
Hayden Cleaver <rexcleaver90@gmail.com>
2022-06-09 18:40:49 -0400

    finished for loop for isCorrect

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ function isCorrect(index, QandA) {
   } else {
     return 0;
   }
+
 }
 
 let questionsAndAnswers = [['Is Josh\'s last name spelled "Frazier"?', 'n'],
@@ -36,19 +37,18 @@ let correctAndIncorrectResponses = [['Correct. It\'s "Frazer".', 'Incorrect! Ple
 
 for (let questionNumber = 0; questionNumber < questionsAndAnswers.length && questionNumber < correctAndIncorrectResponses.length; questionNumber++) {
   const correct = isCorrect(questionNumber, questionsAndAnswers);
+  userScore += correct;
   console.log(correct);
-  // let question = questionsAndAnswers[questionNumber][0];
-  // let correctAnswer = questionsAndAnswers[questionNumber][1];
-  // let correctResponse = correctAndIncorrectResponses[questionNumber][0];
-  // let incorrectResponse = correctAndIncorrectResponses[questionNumber][1];
-  // let userAnswer = prompt(`${question}\n Enter "Y" or "y" for Yes, "N" or "n" for No`).toLowerCase();
-  // if (userAnswer === correctAnswer) {
-  //   alert(correctResponse);
-  //   userScore += 1;
-  // }
-  // else {
-  //   alert(incorrectResponse);
-  // }
+  let correctResponse = correctAndIncorrectResponses[questionNumber][0];
+  let incorrectResponse = correctAndIncorrectResponses[questionNumber][1];
+
+  if (correct){
+    alert(correctResponse);
+  }
+
+  else {
+    alert(incorrectResponse);
+  }
 }
 
 let numberAnswer = Math.floor(Math.random() * 10) + 1;
